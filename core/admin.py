1e80b4777585d1af5b9fcb24b0967f5c827daa81
Samuel-Tefera <samuelteferab@gmail.com>
2024-11-28 11:55:49 +0300

    add User model to admin dashboard

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
+"""Admin Site"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+    list_filter = ['user_type', 'gender']
+
+admin.site.register(User, UserAdmin)
