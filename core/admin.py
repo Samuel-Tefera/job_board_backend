f3cb3eb1b42ec81792ec43c6dfdfa41e42840fc0
Samuel-Tefera <samuelteferab@gmail.com>
2024-11-30 10:05:46 +0300

    add job category model to admin site

--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,15 @@
 
 from django.contrib import admin
 
-from .models import User
+from .models import (
+        User,
+        JobCategory,
+    )
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email']
     list_filter = ['user_type', 'gender']
 
+
 admin.site.register(User, UserAdmin)
+admin.site.register(JobCategory)
