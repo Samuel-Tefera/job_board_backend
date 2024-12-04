98c1abcf9f1cb7d5a0d92a56fa1f87544fd09f7d
Samuel-Tefera <samuelteferab@gmail.com>
2024-12-04 12:00:15 +0300

    register job model od admin dashboard

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,9 @@ from django.contrib import admin
 from .models import (
         User,
         JobCategory,
+        Job
     )
+
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email']
@@ -16,5 +18,11 @@ class JobCategoryAdmin(admin.ModelAdmin):
     list_display = ['category_name', 'created_at']
 
 
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['status', 'type', 'job_category']
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(JobCategory, JobCategoryAdmin)
+admin.site.register(Job, JobAdmin)
