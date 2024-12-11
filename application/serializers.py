031fbf1651aaa564b4cb9c9b412bfcf17fcbd9a3
Samuel-Tefera <samuelteferab@gmail.com>
2024-12-11 11:08:12 +0300

    add application and status update serializer

---
+++ application/serializers.py
@@ -0,0 +1,23 @@
+"""A serializer for application model"""
+
+from rest_framework import serializers
+
+from core.models import Application
+
+
+class ApplicationSerilalizers(serializers.ModelSerializer):
+    """Application Serilalizer"""
+    class Meta:
+        model = Application
+        fields = ['id', 'job', 'applicant', 'status', 'created_at', 'updated_at']
+        read_only_fileds = ['id', 'created_at', 'updated_at', 'status']
+
+
+class UpdateApplicationStatus(serializers.ModelSerializer):
+    """Serielizer to update status of application"""
+    class Meta:
+        model = Application
+        fields = ['status']
+        extra_kwargs = {
+            'status' : {'required' : True, 'choice' : ['acp', 'rej']}
+        }
