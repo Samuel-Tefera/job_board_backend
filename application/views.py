583cee8135cc7e0bdc80e1fb2516714ef44f177c
Samuel-Tefera <samuelteferab@gmail.com>
2024-12-11 11:49:34 +0300

    create api view to manage job applications

--- application/views.py
+++ application/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+"""API views for application model."""
 
-# Create your views here.
+from rest_framework import generics, permissions, authentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from core.models import Application
+from .serializers import ApplicationSerializers, UpdateApplicationStatus
+
+
+class ApplicationCreateView(generics.CreateAPIView):
+    """API view for create application."""
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializers
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def perform_create(self, serializer):
+        serializer.save(applicant=self.request.user)
+
+
+class ApplicationDetailView(generics.RetrieveAPIView):
+    """API view for detail application."""
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializers
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+
+class JobApplicationListView(generics.ListAPIView):
+    """API view for job applications list"""
+    serializer_class = ApplicationSerializers
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def get_queryset(self):
+        job_id = self.kwargs.get('job_id')
+        return Application.objects.filter(job=job_id)
+
+
+class ApplicationUpdateStatusView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def patch(self, request, pk):
+        application = Application.objects.get(id=pk)
+        if application.job.poster_id != request.user:
+            return Response({'error' :
+                'Not authorized to update this application.'}, status=403)
+
+        serializer = UpdateApplicationStatus(application, data=request.data, partail=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
