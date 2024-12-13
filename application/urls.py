5042c8a308f062fa4946b6e8adfeaefd14b087ca
Samuel-Tefera <samuelteferab@gmail.com>
2024-12-13 23:16:22 +0300

    fix typo on urls

--- application/urls.py
+++ application/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('applications', views.ApplicationCreateView.as_view(),
+    path('applications/', views.ApplicationCreateView.as_view(),
          name='application-create'),
     path('applications/<int:pk>/', views.ApplicationDetailView.as_view(),
          name='application-detail'),
     path('applications/<int:pk>/update-status/', views.ApplicationUpdateStatusView.as_view(),
          name='application-update_status'),
-    path('jobs/<int:jon_id>/applications/', views.JobApplicationsListView.as_view(),
+    path('jobs/<int:job_id>/applications/', views.JobApplicationsListView.as_view(),
          name='job-appications-list' )
 ]
